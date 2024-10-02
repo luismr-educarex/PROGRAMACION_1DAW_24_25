34a7622448e1909ffd92a2234da98f1426829d50
profesor <profesor@equipo-00>
2024-10-02 13:32:37 +0200

    nuevos mensajes

--- src/unidades/unidad1/ejemplos/Programa1.java
+++ src/unidades/unidad1/ejemplos/Programa1.java
@@ -2,8 +2,12 @@ package unidades.unidad1.ejemplos;
 
 public class Programa1 {
 
-	public static void main(String[] args) {
-		System.out.println("Primer proyecto");
+	public static void main(String[] argumentos) {
+		
+		System.out.println("primer mensaje");
+		System.out.println("segundo mensaje");
+		System.out.println("tercero mensaje con error");
+		System.out.println("cuarto mensaje");
 
 	}
 
